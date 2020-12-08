766303abcc4d0f52ceaa98a97d8bd9dbcf155fca
V1 MoDzZz <67358940+V1MoDzZz@users.noreply.github.com>
2020-12-08 22:04:09 +0500

    Create sqliscanner.py
    Requirements:
    
    Python3

---
+++ sqliscanner.py
@@ -0,0 +1,42 @@
+import sys
+import urllib
+import urllib.request
+import os
+
+os.system('clear')
+
+sys.ps1 = '\033[01;32m '
+
+print(sys.ps1)
+
+print('''
+ ████████╗██████╗ ██████╗ ██████╗ 
+╚══██╔══╝██╔══██╗╚════██╗╚════██╗
+   ██║   ██████╔╝ █████╔╝ █████╔╝
+   ██║   ██╔══██╗ ╚═══██╗ ╚═══██╗
+   ██║   ██║  ██║██████╔╝██████╔╝
+   ╚═╝   ╚═╝  ╚═╝╚═════╝ ╚═════╝ 
+                                 
+                       
+''')
+
+
+
+url = input("Enter your url with ID paramenters : ")
+
+if (url == "help"):
+      print('''
+
+            for example:
+          http://example.com/index.php?id=1
+
+            ''')
+      sys.exit()
+else:
+     req = urllib.request.urlopen(url + "\'")
+     b10 = req.read()
+     fb100 = b10.decode('utf-8')
+     if "You have an error in your SQL syntax" in fb100:
+         print(url, " is vulnerable")
+     else:
+          print(url, " is not vulnerable")
